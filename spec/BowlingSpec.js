6c83efd61e297c6604f562dd929eb19af190f534
david gray <davidseangray@gmail.com>
2017-05-14 14:36:09 +0100

    added 20 rolls and 10 frames

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -11,10 +11,23 @@ it("calculates a score for the throw one", function() {
   game.throwBallOne();
   expect(game.throwOneScore).toEqual(game.throwOneScore);
 });
-
+// need to stub for random behaviour in the tests
 it("calculates score for the throw two", function() {
   game.throwBallTwo();
   expect(game.throwTwoScore).toEqual(game.throwTwoScore);
+});
+
+it('pushes the throw scores to an array', function () {
+  game.throwBallOne();
+  expect(game.scores).toContain(game.throwOneScore);
+});
+
+it('has 20 throws in a game', function () {
+  for (var i = 0; i < 10; i++) {
+      game.throwBallOne();
+      game.throwBallTwo();
+    }
+  expect(game.scores.length).toEqual(20);
 });
 
 });
--- src/Bowling.js
+++ src/Bowling.js
@@ -1,7 +1,8 @@
 function Game() {
+ this.startingPins = 10;
  this.throwOneScore = 0;
  this.throwTwoScore = 0;
- this.startingPins = 10;
+ this.scores = [];
 
 }
 
@@ -9,10 +10,16 @@ Game.prototype.throwBallOne = function () {
   var pins = this.startingPins;
   var firstScore = Math.floor((Math.random() * pins) + 0);
   this.throwOneScore += firstScore;
+  this.pushScore(firstScore);
 };
 
 Game.prototype.throwBallTwo = function () {
   var throwTwoPins = this.startingPins - this.throwOneScore;
   var secondScore = Math.floor((Math.random() * throwTwoPins) + 0);
   this.throwTwoScore += secondScore;
+  this.pushScore(secondScore);
+};
+
+Game.prototype.pushScore = function (score) {
+  this.scores.push(score);
 };
